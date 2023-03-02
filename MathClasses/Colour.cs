7936865ae605113098f51c35b1f23085e1fd4b25
mike93 <mike.2593@hotmail.com>
2023-03-02 18:23:05 +1300

    Add project files.

---
+++ MathClasses/Colour.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MathClasses
+{
+    public struct Colour
+    {
+        // this will store four bytes representing RGBA in the most to last significant bytes
+        public UInt32 colour;
+
+    }
+}
