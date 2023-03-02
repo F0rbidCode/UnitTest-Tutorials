ce8a4df83bda8d4196cedb6a98be248cc5153aca
mike93 <mike.2593@hotmail.com>
2023-03-02 18:55:22 +1300

    added Vector3 constructer

--- MathClasses/Vector3.cs
+++ MathClasses/Vector3.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,6 +9,19 @@ namespace MathClasses
 {
     public struct Vector3
     {
-        public float x, y, z;
-    }
+        public float x;
+        public float y;
+        public float z;
+
+        public Vector3(float _x, float _y, float _z) : this()
+        {
+            float x = _x;
+            float y = _y;
+            float z = _z;
+        }
+
+    };      
+
+
+    
 }
